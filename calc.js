234edaa8e27794f14bb3e6e57c2ec601d51bacd3
brooklynharden <bharden@udel.edu>
2025-02-03 12:38:32 -0500

    compiled ts

--- calc.js
+++ calc.js
@@ -7,6 +7,19 @@ function appendValue(value) {
 function clearDisplay() {
     display.value = '';
 }
+//function for square root
+function calculateSquareRoot() {
+    var number = parseFloat(display.value);
+    if (isNaN(number)) {
+        display.value = 'Error'; //if inpu not a number show error
+    }
+    else if (number < 0) {
+        display.value = 'Error'; //no sqroot for negative numbers
+    }
+    else {
+        display.value = Math.sqrt(number).toString(); //calculate sq and show result
+    }
+}
 //function to backspace
 function backspace() {
     display.value = display.value.slice(0, -1);
