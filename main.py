9e637c659e93e2d4cc4f10263d4de165ad9114db
brooklynharden <bharden@udel.edu>
2025-02-03 12:08:01 -0500

    trying to make the calculator with typescript now

--- main.py
+++ main.py
@@ -1,57 +1,57 @@
-#Addition Function
-def add(num1, num2):
-    sum = num1 + num2
-    return sum
+# #Addition Function
+# def add(num1, num2):
+#     sum = num1 + num2
+#     return sum
 
-#Subtraction Function
-def subtract(num1, num2):
-    sum = num1 - num2
-    return sum
-
-
-#Division Function
-def divide(num1, num2):
-    sum = num1 / num2
-    return sum
+# #Subtraction Function
+# def subtract(num1, num2):
+#     sum = num1 - num2
+#     return sum
 
 
-#Modulo Function
-def mod(num1, num2):
-    sum = num1%num2
-    return sum
+# #Division Function
+# def divide(num1, num2):
+#     sum = num1 / num2
+#     return sum
 
-#Multiplication Function
-def multiply(num1, num2):
-    sum = num1*num2
-    return sum
 
-print("Please Selection Operate: -\n" \
-    "1. Add\n"
-    "2. Subtract\n"
-    "3. Divide\n"
-    "4. Modulo\n"
-    "5. Multiply\n")
+# #Modulo Function
+# def mod(num1, num2):
+#     sum = num1%num2
+#     return sum
 
-#User Input
-userSelect = int(input("Select Operation 1, 2, 3, 4, 5: "))
+# #Multiplication Function
+# def multiply(num1, num2):
+#     sum = num1*num2
+#     return sum
 
-Number_1 = int(input("First Number: "))
-Number_2 = int(input("Second Number: "))
+# print("Please Selection Operate: -\n" \
+#     "1. Add\n"
+#     "2. Subtract\n"
+#     "3. Divide\n"
+#     "4. Modulo\n"
+#     "5. Multiply\n")
 
-if userSelect == 1:
-   print(Number_1, "+",Number_2,"=", add(Number_1, Number_2))
+# #User Input
+# userSelect = int(input("Select Operation 1, 2, 3, 4, 5: "))
 
-elif userSelect == 2:
-    print(Number_1, "-",Number_2,"=",subtract(Number_1, Number_2))
+# Number_1 = int(input("First Number: "))
+# Number_2 = int(input("Second Number: "))
 
-elif userSelect == 3:
-    print(Number_1, "/",Number_2,"=", divide(Number_1,Number_2))
+# if userSelect == 1:
+#    print(Number_1, "+",Number_2,"=", add(Number_1, Number_2))
 
-elif userSelect == 4:
-    print(Number_1, "+%",Number_2,"=", mod(Number_1,Number_2))
+# elif userSelect == 2:
+#     print(Number_1, "-",Number_2,"=",subtract(Number_1, Number_2))
 
-elif userSelect == 5:
-    print(Number_1, "*",Number_2,"=", multiply(Number_1,Number_2))
+# elif userSelect == 3:
+#     print(Number_1, "/",Number_2,"=", divide(Number_1,Number_2))
 
-else:
-    print("Invalid Number")
+# elif userSelect == 4:
+#     print(Number_1, "+%",Number_2,"=", mod(Number_1,Number_2))
+
+# elif userSelect == 5:
+#     print(Number_1, "*",Number_2,"=", multiply(Number_1,Number_2))
+
+# else:
+#     print("Invalid Number")
