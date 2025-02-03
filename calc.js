e5750db8589e5acfb1971353768f10d5c70e98cb
brooklynharden <bharden@udel.edu>
2025-02-03 12:21:53 -0500

    added backspace function to calc.js by compiling ts (tsc calc.ts)

--- calc.js
+++ calc.js
@@ -7,6 +7,10 @@ function appendValue(value) {
 function clearDisplay() {
     display.value = '';
 }
+//function to backspace
+function backspace() {
+    display.value = display.value.slice(0, -1);
+}
 //function to calculate and display result
 function calculateResult() {
     try {
